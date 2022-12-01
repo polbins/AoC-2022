8b22ca973242c9a131519452155df2e3795f7475
Paul Intal <pintal@squareup.com>
2022-12-01 14:49:50 -0500

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Long {
+    var current: Long = 0
+    var highest: Long = 0
+
+    for (s in input) {
+      current = s.toLongOrNull()?.let {
+        current + it
+      } ?: 0
+
+      if (current > highest) {
+        highest = current
+      }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    return highest
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01_test")
+  check(part1(testInput) == 24000L)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readInput("Day01")
+  println(part1(input))
 }
