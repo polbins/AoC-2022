b826ab087b20bd00b16d107aeedf91ce53e13331
Paul Intal <pintal@squareup.com>
2022-12-02 11:38:05 -0500

    day 1 - part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -16,10 +16,36 @@ fun main() {
     return highest
   }
 
+  fun part2(input: List<String>): Set<Int> {
+    var current = 0
+    val sums = sortedSetOf(0)
+
+    for (s in input) {
+      val num = s.toIntOrNull()
+      current = if (num != null) {
+        current + num
+      } else {
+        if (current > sums.first()) {
+          sums.add(current)
+
+          if (sums.size > 3) {
+            sums.pollFirst()
+          }
+        }
+        0
+      }
+    }
+
+    return sums
+  }
+
   // test if implementation meets criteria from the description, like:
   val testInput = readInput("Day01_test")
   check(part1(testInput) == 24000L)
 
-  val input = readInput("Day01")
-  println(part1(input))
+  val input1 = readInput("Day01")
+  println(part1(input1))
+
+  val input2 = readInput("Day01")
+  println(part2(input2).sum())
 }
